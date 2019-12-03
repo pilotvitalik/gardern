9d4faf7b25cf1df3ccb519932d3b94cfc4a67d0e
pilotvitalik <itvkip@yandex.ru>
2019-12-03 08:33:26 +0300

    Added updated project files. Webpack removed

---
+++ js/callback.js
@@ -0,0 +1,58 @@
+let btn = document.querySelectorAll('.callback');
+let modalWindow = document.querySelector('.modal');
+let body = document.querySelector('body');
+let closeBtn = document.querySelector('.closeBtn');
+let closeImgPlane = document.querySelector('.closeImgPlane');
+let modalImg = document.querySelector('.modalImg');
+let showImgPlane = document.querySelector('.showImgPlane');
+
+for(let i = 0; i < btn.length; i++){
+		btn[i].onclick = () => {
+		modalWindow.classList.toggle('active');
+		body.style.pointerEvents = 'none';
+	}
+}
+
+$('#modalForm').submit(function(event) {
+	if($('#modalName')[0].value === ''){
+		event.preventDefault();
+		alert('Введите имя');
+	} else {
+		if($('#modalPhone')[0].value === ''){
+			event.preventDefault();
+			alert('Введите номер телефона')
+		} else {
+			$.ajax({
+				type: 'POST',
+				url: 'mail.php',
+				data: $(this).serialize()
+			}).done(() => {
+				alert('Спасибо за заявку! Скоро мы с Вами свяжемся.')
+			})
+			$('#modalName')[0].value = '';
+			$('#modalPhone')[0].value = '';
+			modalWindow.classList.toggle('active');
+			body.style.pointerEvents = 'auto';
+			return false;
+		}
+		}
+})
+	
+closeBtn.onclick = () => {
+	modalWindow.classList.toggle('active');
+	body.style.pointerEvents = 'auto';
+	body.style.overflowY = 'auto';
+}
+
+showImgPlane.onclick = () => {
+	modalImg.classList.toggle('active');
+	body.style.pointerEvents = 'none';
+	body.style.overflow = 'hidden';
+}
+
+closeImgPlane.onclick = () => {
+	modalImg.classList.toggle('active');
+	body.style.pointerEvents = 'auto';
+	body.style.overflowY = 'auto';
+}
+
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './css/1200.css'
-import './css/1000.css';
-import './css/768.css';
-import './css/320.css';
-import './js/slider.js';
-import './js/rewind.js';
-import './js/callback.js';
-import './js/request.js';
